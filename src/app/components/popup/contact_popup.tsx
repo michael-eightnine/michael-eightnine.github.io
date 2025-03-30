65fd95861394f861d764a0cc2e3517f79cc0fbed
Michael Smith <msmith0892@gmail.com>
2025-03-30 09:34:37 -0500

    Adding eternal devotion popup link

--- src/app/components/popup/contact_popup.tsx
+++ src/app/components/popup/contact_popup.tsx
@@ -12,7 +12,6 @@ const ContactPopup = (props: Omit<GenericPopupProps, 'children' | 'title'>) => {
             <span>Contact:</span>
             <a href="mailto:msmith0892@gmail.com">msmith0892[at]gmail</a>
           </li>
-
           <li>
             <span>Resume:</span>
             <a
@@ -41,6 +40,16 @@ const ContactPopup = (props: Omit<GenericPopupProps, 'children' | 'title'>) => {
               target="_blank"
             >
               For connecting
+            </a>
+          </li>
+          <li>
+            <span>Paintings:</span>
+            <a
+              href="/eternal-devotion"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              For viewing
             </a>
           </li>
         </ul>
